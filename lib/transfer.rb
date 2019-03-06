07f899dc9dcc6950cfe14c4b011cdba26f80c5b0
Lizzie Chan <lchan217@gmail.com>
2019-03-06 23:00:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,21 +12,5 @@ class Transfer
     @sender.valid? 
     @receiver.valid? 
   end 
-  def execute_transaction
-    if @status == "pending" && @amount < @sender.balance
-       @status = "complete"
-       @sender.balance -= @amount 
-       @receiver.balance +=@amount
-    else 
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end 
-  end 
-  def reverse_transfer 
-    if @status == "complete"
-      @status = "reversed"
-      @sender.balance += @amount 
-      @receiver.balance -=@amount
-    end
-  end
+  
 end
